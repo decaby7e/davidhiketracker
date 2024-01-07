d3cc58e670cab0afbb165de7487c47ea4eb97945
decaby7e <27081624+decaby7e@users.noreply.github.com>
2024-01-07 02:23:57 -0500

    doc: comments

--- src/components/TrackerBar.jsx
+++ src/components/TrackerBar.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+// NOTE: idea: have the bar run from the top to the bottom of the page and set
+//             the timer to show how long until the next milestone in the bar is
+//             reached. add silly GIFs as markers?
 const TrackerBar = ({ percentComplete }) => {
   return (
     <>

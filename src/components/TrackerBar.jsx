1c6347962894fa9a0b359512b2d489f36c6f8cc1
decaby7e <27081624+decaby7e@users.noreply.github.com>
2024-02-06 13:53:13 -0800

    tweaks

--- src/components/TrackerBar.jsx
+++ src/components/TrackerBar.jsx
@@ -6,11 +6,11 @@ import React from "react";
 const TrackerBar = ({ percentComplete }) => {
   return (
     <>
-      <div className="flex">
-        <div className="flex border-2 border-green-600 rounded-lg flex-grow h-[3rem]">
+      <div className="flex flex-col h-screen">
+        <div className="flex border-2 border-green-600 rounded-lg flex-grow w-[3rem]">
           <div
             className={`bg-green-600 border-2 border-green-600 rounded-lg`}
-            style={{ width: `${percentComplete}%` }}
+            style={{ width: "100%", height: `${percentComplete}%` }}
           ></div>
         </div>
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,31 +8,46 @@ const pageStyles = {
   color: "#232129",
   padding: 96,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  textAlign: "center",
 };
 
 const IndexPage = () => {
   const percentComplete = 0; // TODO: implement
-  const startDate = "2024-02-14"; // TODO: get real value
+  const startDate = "2024-02-14";
 
   return (
     <main style={pageStyles}>
-      <h1 className="text-[3rem] mb-[4rem]">
-        Holy Fuck! David Dilmore is Hiking the Appalachian Trail!
+      <h1 className="text-[3rem] mb-[2rem]">
+        Holy Fuck!{" "}
+        <span className="hover:underline">
+          <a
+            className="text-transparent bg-clip-text bg-gradient-to-r from-pink-700 to-blue-700"
+            href="https://www.instagram.com/david.dilmore/"
+          >
+            David Dilmore
+          </a>
+        </span>{" "}
+        is Hiking the Appalachian Trail!
       </h1>
-      {/* TODO: add a little description here */}
-      <h2 className="text-[2.5rem] mt-[2rem] mb-[1rem] font-bold">
-        Time Remaining Till Start
-      </h2>
-      <CountdownTimer tillDate={new Date(Date.parse(startDate))} />
-      <h2 className="text-[2.5rem] mt-[2rem] mb-[1rem] font-bold">Progress</h2>
-      <TrackerBar percentComplete={percentComplete} />
-      <br />
-      <br />
-      <br />
       <p className="italic">
         Come back later for more features! (gps tracking, links to david's trail
         vlogs, milestones, etc.)
       </p>
+      <br />
+      <br />
+      <br />
+      <div className="flex flex-col">
+        <h2 className="text-[2.5rem] mt-[2rem] mb-[1rem] font-bold">
+          Time Remaining Till Start
+        </h2>
+        <CountdownTimer tillDate={new Date(Date.parse(startDate))} />
+      </div>
+      {/* TODO: Uncomment after start */}
+      {/* <h2 className="text-[2.5rem] mt-[2rem] mb-[1rem] font-bold">Progress</h2>
+      <TrackerBar percentComplete={percentComplete} /> */}
+      <br />
+      <br />
+      <br />
     </main>
   );
 };
